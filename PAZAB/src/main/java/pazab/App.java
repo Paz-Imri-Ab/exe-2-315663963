128c4f714519049a4185cb689021a8f204dd814d
pazimriabutbul <pazimriabutbul@192.168.1.25>
2024-01-21 20:48:11 +0200

    version-1

--- PAZAB/src/main/java/pazab/App.java
+++ PAZAB/src/main/java/pazab/App.java
@@ -6,9 +6,52 @@ package pazab;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-    	
-        System.out.println( "Hello World!" );
-    }
+	
+	
+	
+	public String numberToChar(int num)
+	{
+		String result;
+		
+		if(num<1) 
+		{
+			result = "Error";
+		}
+		
+		else if(num >= 1 && num <=49) 
+		{
+			result = "F";
+		}
+		
+		else if(num >= 50 && num <=59) 
+		{
+			result = "E"; 
+		}
+		
+		else if(num >= 60 && num <= 69) 
+		{
+			result="D";
+		}
+		
+		else if(num >= 70 && num <= 79) 
+		{
+			result = "C";
+		}
+		
+		else if(num >= 80 && num <=89) 
+		{
+			result = "B";
+		}
+		
+		else if(num >= 90 && num <= 100) 
+		{
+			result = "A";
+		}
+		
+		else result = "Error";
+		
+		return result;
+	}
+    
+    
 }
